cf680de97d665d869c017e67209c0bbed835089c
NetBeans Projects <shreeja.926@gmail.com>
2021-12-28 20:00:34 +0530

    First commit

---
+++ Demo.java
@@ -0,0 +1,20 @@
+package demo;
+
+import java.util.*;
+
+interface XList {
+
+    static XList sampledemo()
+    {
+        System.out.println("Shreeja");
+        return null;
+    }
+
+}
+public class Demo  {
+
+    public static void main(String[] args) {
+        System.out.println(XList.sampledemo());
+
+    }
+}
